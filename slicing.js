e3a531c12bb64ad9067646164d69532198150815
aradhyasakalley <sakalleyaradhya@gmail.com>
2023-07-11 13:24:18 +0530

    Algorithm integration

---
+++ slicing.js
@@ -0,0 +1,15 @@
+let actualText = 'Hello there how are you hope you are doing well';
+
+function sliceActualText(text, spokenTextLength) {
+  const actualTextArray = text.split(' ');
+  const leftSlice = actualTextArray.slice(0, spokenTextLength).join(' ');
+  const rightSlice = actualTextArray.slice(spokenTextLength).join(' ');
+  console.log('Left slice:', leftSlice);
+  console.log('Right slice:', rightSlice);
+  actualText = rightSlice; // Update the global variable actualText
+//   console.log(actualText);
+}
+
+sliceActualText(actualText, 3);
+console.log(actualText);
+sliceActualText(actualText, 2);
